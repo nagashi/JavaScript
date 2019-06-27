813cd73a88f27a7f180db2b0e9399317c2308fd0
Charles ORiley <ceoriley@gmail.com>
2019-06-27 01:43:51 -0500

    Corrected algorithm for temperature conversion

--- cel2fah.js
+++ cel2fah.js
@@ -2,5 +2,7 @@ var readlineSync = require('readline-sync');
 
 var degrees = readlineSync.question('Enter degrees in Celsius: ');
 var degreesNum = Number(degrees);
-var degreesFahrenheit = degreesNum * 1.8 + 32;
+var degreesFahrenheit = ( (degreesNum * 1.8) + 32) ;
+
+
 console.log('It is ' + degreesFahrenheit + ' degrees Fahrenheit!');
--- fah2cel.js
+++ fah2cel.js
@@ -3,4 +3,5 @@ var readlineSync = require('readline-sync');
 var degrees = readlineSync.question('Enter degrees in Fahrenheheit ');
 var degreesNum = Number(degrees);
 var degreesFahrenheit = ( (degreesNum -32) / 1.8 ) ;
+
 console.log('It is ' + degreesFahrenheit + ' degrees Celsius!');
